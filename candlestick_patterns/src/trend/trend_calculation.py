ee5598d0a8c740889d3af9ec68d51b48b55327a5
wnotermans <woutnotermans1@gmail.com>
2025-03-01 23:23:36 +0100

    docs: :memo: Documentation for the new way of trend calculation

--- candlestick_patterns/src/trend/trend_calculation.py
+++ candlestick_patterns/src/trend/trend_calculation.py
@@ -8,6 +8,31 @@ import pandas as pd
 def calculate_rolling_average(
     ser: pd.Series, averaging_method: str = "MA", span: int = 5
 ) -> pd.Series:
+    """
+    Calculate different rolling averages.
+
+    Parameters
+    ----------
+    ser : pd.Series
+        Data to be averaged.
+    averaging_method : str, optional, default `"MA"`
+        - `"MA"`: simple moving average
+        - `"WMA"`: weighted moving average with linearly decreasing weights
+        - `"EWMA"`: exponentially weighted moving average, with `alpha=2/(1+span)`
+    span : int, optional, default 5
+        How many data points are included in (weighted) moving average, also governs the
+        alpha used in the exponentially weighted moving average.
+
+    Returns
+    -------
+    pd.Series
+        The averaged data.
+
+    Raises
+    ------
+    ValueError
+        If an unknown averaging method is given.
+    """
     if averaging_method == "MA":
         return ser.rolling(span).mean()
     if averaging_method == "WMA":
@@ -27,6 +52,38 @@ def calculate_trend(
     decision_method: str = "monotonic",
     **kwargs,
 ) -> pd.DataFrame:
+    """
+    Calculate short-term trend of data.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe with at least the column `"close"`.
+    averaging_method : str, optional, default "MA"
+        Method used to calculate a short-term average.
+        - `"MA"`: simple moving average
+        - `"WMA"`: weighted moving average with linearly decreasing weights
+        - `"EWMA"`: exponentially weighted moving average, with `alpha=2/(1+span)`
+    span : int, optional, default 5
+        Span over which the short-term average is calculated.
+    decision_method : str, optional, default `"monotonic"`
+        Method that decides the trend.
+        - `"monotonic"`: based on consecutive in/decreases of the short-term average.
+        Additional kwarg `consecutive` can be passed to decide the number of needed
+        in/decreases.
+    **kwargs : dict, optional
+        Additional arguments to be passed into the decision method.
+
+    Returns
+    -------
+    pd.DataFrame
+        Original dataframe with an additional column `"trend"`.
+
+    Raises
+    ------
+    ValueError
+        When an unknown decision method is passed.
+    """
     t = time.perf_counter()
 
     consecutive = kwargs.get("consecutive", 7)
