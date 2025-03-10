c0ea7fd609e540cdf2676b801e6078ed45d936ab
wnotermans <woutnotermans1@gmail.com>
2025-03-10 23:26:00 +0100

    ✨ feat: Replace wiener data with geometric Brownian motion

---
+++ candlestick_patterns/src/GBM/GBM.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+
+
+def geometric_brownian_motion(
+    N: int, S0: float = 100, mu: float = 0, sigma: float = 1
+) -> np.ndarray:
+    """
+    Wiener process of size N.
+
+    Generates a Wiener process of size N. The process starts at zero, and then at each
+    step a normally distributed disturbance is added to it. The optional parameter mu
+    can be passed to create a drift. Standard deviation is always 1.
+
+    Parameters
+    ----------
+    N : int
+        How many data points to generate.
+    S0 : float, optional, default 100
+        The initial value.
+    mu : float, optional, default 0
+        Drift parameter. Defaults to zero meaning no drift.
+    sigma : float, optional, default 1
+        Volatility of the process.
+
+    Returns
+    -------
+    np.ndarray
+        The Wiener process.
+    """
+    rng = np.random.default_rng(1)
+    dt = 1 / (365 * 23400)
+    T = N * dt
+    t = np.linspace(0, T, N)
+    W = rng.standard_normal(size=N)
+    W = np.cumsum(W) * np.sqrt(dt)
+    return S0 * np.exp((mu - 0.5 * sigma**2) * t + sigma * W)
+
+
+time_idx = pd.concat(
+    [
+        pd.date_range(
+            start=f"{date} 09:30:00", end=f"{date} 15:59:59", freq="s"
+        ).to_series()
+        for date in pd.date_range(start="2001-01-01", end="2020-12-31")
+    ]
+)
+
+GBM_series = pd.Series(geometric_brownian_motion(len(time_idx)), time_idx)
+# GBM_series = pd.Series(
+#     geometric_brownian_motion(len(time_idx), mu=0.1), time_idx
+# )  # drift
+GBM_df = GBM_series.resample("1min", label="right").ohlc()
+GBM_df["datetime"] = GBM_df.index
+GBM_df["volume"] = 0
+GBM_df.to_parquet("../data/Wiener.parquet", compression="lz4")
