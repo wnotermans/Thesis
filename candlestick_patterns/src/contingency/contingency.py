6259ffd34786f6a8943c6f2d7ee115e131cfa680
wnotermans <woutnotermans1@gmail.com>
2025-03-19 23:21:47 +0100

    ♻️ refactor(pandas): Make more efficient use of pandas
    Also rename some functions/parameters for clarity.

--- candlestick_patterns/src/contingency/contingency.py
+++ candlestick_patterns/src/contingency/contingency.py
@@ -1,15 +1,10 @@
-import csv
 import os
 
 import pandas as pd
 
 
-def get_last_line_values(file_path: str) -> tuple[int, int]:
-    with open(file_path, newline="", encoding="utf-8") as f:
-        reader = csv.reader(f)
-        last_line = None
-        for row in reader:
-            last_line = row
+def get_buy_sell(file_path: str) -> tuple[int, int] | tuple[None, None]:
+    last_line = pd.read_csv(file_path).iloc[-1]
     return (
         (
             "".join(filter(str.isnumeric, last_line[0])),
@@ -23,7 +18,7 @@ def get_last_line_values(file_path: str) -> tuple[int, int]:
     )
 
 
-def find_csv_files(directory: str) -> str:
+def list_csv_files(directory: str) -> list[str] | list:
     csv_files = []
     for root, _, files in os.walk(directory):
         for file in files:
@@ -32,27 +27,26 @@ def find_csv_files(directory: str) -> str:
     return csv_files
 
 
-def update_contingency_table(directory: str) -> pd.DataFrame:
-    contingency_table = pd.DataFrame(columns=["File", "Value 1", "Value 2"])
+def make_contingency_table(directory: str) -> pd.DataFrame:
+    csv_files = list_csv_files(directory)
 
-    csv_files = find_csv_files(directory)
     # TODO filtering by name here, regex?
 
-    for file in csv_files:
-        value1, value2 = get_last_line_values(file)
-        contingency_table = pd.concat(
-            [
-                contingency_table,
-                pd.DataFrame(
-                    {"File": file, "Value 1": value1, "Value 2": value2}, index=[0]
-                ),
-            ],
-            ignore_index=True,
-        )
+    contingency_records = []
 
-    return contingency_table
+    for filename in csv_files:
+        buy, sell = get_buy_sell(filename)
+        contingency_records.append(
+            {
+                "Filename": filename.removeprefix("../data/summaries\\"),
+                "# sign. buy signals": buy,
+                "# sign. sell signals": sell,
+            }
+        )
+    contingency_table = pd.DataFrame.from_records(contingency_records)
+    return contingency_table.set_index("Filename")
 
 
 directory = "../data/summaries"
-contingency_table = update_contingency_table(directory)
+contingency_table = make_contingency_table(directory)
 print(contingency_table)
