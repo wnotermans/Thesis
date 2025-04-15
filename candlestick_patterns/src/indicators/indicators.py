70311a05a0ac72e1870ccd6f8465bb99dd5b0773
wnotermans <woutnotermans1@gmail.com>
2025-04-15 23:40:31 +0200

    ✨ feat: Add final technical indicators

--- candlestick_patterns/src/indicators/indicators.py
+++ candlestick_patterns/src/indicators/indicators.py
@@ -117,6 +117,28 @@ def average_directional_movement_index(
     return 100 * adx.rolling(window).mean()
 
 
+def Bollinger_bands(df: pd.DataFrame, *, indicator_kwargs: dict) -> tuple[pd.Series]:
+    """
+    Calculates Bollinger bands. Uses 2 std. dev. moves.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame with OHLC data.
+    indicator_kwargs : dict
+        ``window``: controls the rolling window size.
+
+    Returns
+    -------
+    tuple[pd.Series]
+        Returns lower band, moving average and upper band.
+    """
+    window = indicator_kwargs["window"]
+    ma = df["close"].rolling(window).mean()
+    sd = df["close"].rolling(window).std()
+    return ma - 2 * sd, ma, ma + 2 * sd
+
+
 def detrended_price_oscillator(
     df: pd.DataFrame, *, indicator_kwargs: dict
 ) -> pd.Series:
@@ -136,6 +158,30 @@ def moving_average_convergence_divergence(
     EMA_long = df["close"].ewm(span=span_2, adjust=False).mean()
     MACD = EMA_short - EMA_long
     return MACD, MACD.ewm(span=span_3, adjust=False).mean()
+
+
+def money_flow_index(df: pd.DataFrame, *, indicator_kwargs: dict) -> pd.Series:
+    """
+    Calculates the money flow index (MFI).
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame with OHLC data.
+    indicator_kwargs : dict
+        ``window``: controls the window size.
+
+    Returns
+    -------
+    pd.Series
+        The MFI indicator.
+    """
+    window = indicator_kwargs["window"]
+    money_flow = (df["high"] + df["low"] + df["close"]) / 3 * df["volume"]
+    money_flow_shift = money_flow.shift()
+    pos_money_flow = (money_flow > money_flow_shift).rolling(window).sum()
+    neg_money_flow = (money_flow < money_flow_shift).rolling(window).sum()
+    return 100 * pos_money_flow / (pos_money_flow + neg_money_flow)
 
 
 def momentum(df: pd.DataFrame, *, indicator_kwargs: dict) -> pd.Series:
@@ -168,3 +214,93 @@ def vortex(df: pd.DataFrame, *, indicator_kwargs: dict) -> pd.Series:
     VI_plus = VM_plus.rolling(window).sum() / tr_sum
     VI_minus = VM_minus.rolling(window).sum() / tr_sum
     return VI_plus, VI_minus, VI_plus - VI_minus
+
+
+def Williams_R(df: pd.DataFrame, *, indicator_kwargs: dict) -> pd.Series:
+    """
+    Calculates Williams' %R (%R).
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame with OHLC data.
+    indicator_kwargs : dict
+        ``window``: controls the window size.
+
+    Returns
+    -------
+    pd.Series
+        The %R indicator.
+    """
+    window = indicator_kwargs["window"]
+    rolling_high = df["high"].rolling(window).max()
+    rolling_low = df["low"].rolling(window).min()
+    return -100 * (rolling_high - df["close"]) / (rolling_high - rolling_low)
+
+
+def parabolic_SAR(df: pd.DataFrame, *, indicator_kwargs: dict) -> list:
+    """
+    Calculates the parabolic stop and reverse indicator (PSAR).
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame with OHLC data.
+    indicator_kwargs : dict
+        ``step``: how much to increment the acceleration factor by.
+
+    Returns
+    -------
+    list
+        The PSAR indicator.
+    """
+    step = indicator_kwargs["step"]
+    max_accel_factor = indicator_kwargs["max_accel_factor"]
+
+    length = len(df)
+    high = list(df["high"])
+    low = list(df["low"])
+    psar = list(df["close"])  # faster than casting/copying to numpy
+    up_trend = True
+    accel_factor = step
+    max_high = high[0]
+    min_low = low[0]
+
+    for i in range(2, length):
+        if up_trend:
+            psar[i] = psar[i - 1] + accel_factor * (max_high - psar[i - 1])
+        else:
+            psar[i] = psar[i - 1] + accel_factor * (min_low - psar[i - 1])
+        reverse = False
+
+        if up_trend:
+            if low[i] < psar[i]:
+                up_trend = False
+                reverse = True
+                psar[i] = max_high
+                min_low = low[i]
+                accel_factor = step
+
+        elif high[i] > psar[i]:
+            up_trend = True
+            reverse = True
+            psar[i] = min_low
+            max_high = high[i]
+            accel_factor = step
+
+        if not reverse:
+            if up_trend:
+                if high[i] > max_high:
+                    max_high = high[i]
+                    accel_factor = min(accel_factor + step, max_accel_factor)
+                psar[i] = min(psar[i], low[i - 1])
+                psar[i] = min(psar[i], low[i - 2])
+
+            else:
+                if low[i] < min_low:
+                    min_low = low[i]
+                    accel_factor = min(accel_factor + step, max_accel_factor)
+                psar[i] = max(psar[i], high[i - 1])
+                psar[i] = max(psar[i], high[i - 2])
+
+    return psar
