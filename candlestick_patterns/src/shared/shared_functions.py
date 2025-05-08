e1c67268bafec840f1bd4fa20646ed1a6ae289e9
wnotermans <woutnotermans1@gmail.com>
2025-05-08 22:53:45 +0200

    🎨 style: Make status bar printer more universal

--- candlestick_patterns/src/shared/shared_functions.py
+++ candlestick_patterns/src/shared/shared_functions.py
@@ -1,25 +1,22 @@
-def print_status_bar(pattern_name: str, i: int, total_patterns: int) -> None:
+def print_status_bar(message: str, i: int, total_patterns: int) -> None:
     """
     Prints out the status bar (function name, progress bar and count), overwriting the
     previous status bar.
 
     Parameters
     ----------
-    pattern_name : str
-        Name of the pattern being evaluated.
+    message : str
+        Message to be printed.
     i : int
-        Current iteration number.
+        Current iteration number. Automatically adds +1 for convenience with
+        ``enumerate()``.
     total_patterns : int
         Total number of patterns.
     """
     left_line = (51 * (i + 1) // total_patterns) * "-"
     right_line = (51 - len(left_line)) * "-"
     progress_bar = f"|{left_line}>>{right_line}|"
-    status_bar = (
-        f"Evaluating {pattern_name:<52}"
-        + progress_bar
-        + f"({i + 1:>3}/{total_patterns})"
-    )
+    status_bar = f"{message:<62}" + progress_bar + f"({i + 1:>3}/{total_patterns})"
     print(status_bar, end="\r")
 
 
