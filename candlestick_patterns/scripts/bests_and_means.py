6553fbdebeea452972496e79fa6c99de04126cc5
wnotermans <woutnotermans1@gmail.com>
2025-05-12 22:03:54 +0200

    ♻️ refactor: Slightly change output and filtering approach of best and means

--- candlestick_patterns/scripts/bests_and_means.py
+++ candlestick_patterns/scripts/bests_and_means.py
@@ -19,7 +19,7 @@ COLS = {
 }
 
 
-def bests_and_means(filter_string: str = "") -> None:
+def bests_and_means(include: list[str] = None, exclude: list[str] = None) -> None:
     """
     Print several best and mean statistics.
 
@@ -29,11 +29,17 @@ def bests_and_means(filter_string: str = "") -> None:
         String to filter by. If this string is not in parameters.txt, it is excluded
         from the results.
     """
+    if include is None:
+        include = [""]
+    if exclude is None:
+        exclude = []
     rows, indices = [], []
     for run_name in os.listdir("src/data/runs"):
         with open(f"src/data/runs/{run_name}/parameters.txt") as f:
             index = f.read()
-        if filter_string not in index:
+        if not all(filter_string in index for filter_string in include):
+            continue
+        if any(filter_string in index for filter_string in exclude):
             continue
         indices.append(index)
         rows.append(
@@ -42,16 +48,23 @@ def bests_and_means(filter_string: str = "") -> None:
             .to_list()
         )
     data = pd.DataFrame(rows, index=indices, columns=COLS.keys()).astype(COLS)
+    if n := len(data):
+        print(f"Selected {n} runs that {include = } and {exclude = }.")
+    else:
+        return
 
+    data.loc[data["Best buy win rate"] == "/", "Best buy win rate"] = 50
+    data.loc[data["Best sell win rate"] == "/", "Best sell win rate"] = 50
     data[["Best buy win rate", "Best sell win rate"]] = data[
         ["Best buy win rate", "Best sell win rate"]
     ].apply(lambda x: x.str.slice(stop=5).astype(float))
 
-    print(f"Mean significant buy signals: {data['Significant buy signals'].mean():.2f}")
+    print(f"Significant buy signals: {data['Significant buy signals'].mean().round(2)}")
     print(
-        f"Mean significant sell signals: {data['Significant sell signals'].mean():.2f}"
+        f"Significant sell signals: {data['Significant sell signals'].mean().round(2)}"
     )
-    print(f"Mean non significant signals: {data['Non significant signals'].mean():.2f}")
+    print(f"Non significant signals: {data['Non significant signals'].mean().round(2)}")
+    print(f"Mean number detected: {data['Total number detected'].mean().astype(int)}")
 
     best_buy = data["Best buy pattern"].mode().iloc[0]
     best_buy_idx = data[data["Best buy pattern"] == best_buy].index
@@ -64,13 +77,13 @@ def bests_and_means(filter_string: str = "") -> None:
     print(
         "Most occurring best buy pattern with mean p-value and win rate: "
         f"{best_buy}, "
-        f"{best_buy_mean_pvalue:.4f}, "
+        f"{best_buy_mean_pvalue:.4g}, "
         f"{best_buy_mean_win_rate:.2f}%",
     )
     print(
         "Most occurring best sell pattern with mean p-value and win rate: "
         f"{best_sell}, "
-        f"{best_sell_mean_pvalue:.4f}, "
+        f"{best_sell_mean_pvalue:.4g}, "
         f"{best_sell_mean_win_rate:.2f}%",
     )
 
@@ -83,8 +96,17 @@ def bests_and_means(filter_string: str = "") -> None:
         f"{most_profitable_mean:.2f}"
     )
 
-    print(data["Average profitability score"].describe())
-    print(data["Total number detected"].describe())
+    print(f"Mean profitability: {data['Average profitability score'].mean().round(4)}")
+    print(
+        "Average profitability score per pattern: "
+        f"{
+            data['Average profitability score'].mean()
+            / (
+                data['Significant buy signals'].sum()
+                + data['Significant sell signals'].sum()
+            ):.4g
+        }"
+    )
 
 
 if __name__ == "__main__":
